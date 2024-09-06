abd06bffb8dbaeb4f292c9747e5daddd92b033ae
Sonika Garg <sonikagarg@Sonikas-MBP.lan>
2024-09-06 16:31:38 -0500

    fixed errors in version numbers and added commit to database

--- script.py
+++ script.py
@@ -4,17 +4,17 @@ import datetime
 from packaging.version import Version
 import sys
 
-def create_database():
+def create_database(end, last_date, keyword_string):
     end = datetime.datetime.now()
     start = end - datetime.timedelta(days=30)
     last_date = datetime.datetime(2023, 1, 1)
     cpes = []
     while(start > last_date):
         print("in loop")
-        cpes += (nvdlib.searchCPE(keywordSearch = 'java-merge-sort', key = 'da031268-411b-4cfb-9e67-0f5b5d949299', lastModStartDate=start, lastModEndDate=end))
+        cpes += (nvdlib.searchCPE(keywordSearch = keyword_string, key = 'da031268-411b-4cfb-9e67-0f5b5d949299', lastModStartDate=start, lastModEndDate=end))
         start -= datetime.timedelta(days=30)
         end -= datetime.timedelta(days=30) 
-    cpes += (nvdlib.searchCPE(keywordSearch = 'java-merge-sort', key = 'da031268-411b-4cfb-9e67-0f5b5d949299', lastModStartDate=last_date, lastModEndDate=end))
+    cpes += (nvdlib.searchCPE(keywordSearch = keyword_string, key = 'da031268-411b-4cfb-9e67-0f5b5d949299', lastModStartDate=last_date, lastModEndDate=end))
     cpes = set(cpes)
     print(len(cpes))
     cves = {}
@@ -42,13 +42,14 @@ def create_database():
                 if version:
                     cves[cve.id].append(version)
     print(list(cves.items()))
+    create_database_mysql(cves)
 
     #for k,v in cves.items():
 
 
 def create_database_mysql(cves):
-    conn = mysql.connector.connect(user='XXXX', password='XXXX', host='XXXX', 
-    port='XXXX',database='XXXX')
+    conn = mysql.connector.connect(user='root', password='cool1234', host='localhost', 
+    port='3306',database='cve')
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE Cves (ID varchar(255) NOT NULL, Version varchar(255), PRIMARY KEY (ID));")
     for k,v in cves.items():
@@ -58,10 +59,12 @@ def create_database_mysql(cves):
             cursor.execute(sql, val)
         else:
             v.sort()
-            versions = str(v[0]) + ", " + str(v[:-1])
+            versions = str(v[0]) + ", " + str(v[-1])
+            print(versions)
             sql = "INSERT INTO Cves (ID, Version) VALUES (%s, %s)"
             val = (k, versions)
             cursor.execute(sql, val)
+            conn.commit()
     conn.close()
 
 def containsDigit(version):
@@ -71,7 +74,10 @@ def containsDigit(version):
 def main():
     print("here")
     #create_database()
-    create_database_mysql()
+    end = datetime.datetime.now()
+    last_date = datetime.datetime(2023, 1, 1)
+    keyword_string = 'java-merge-sort'
+    create_database(end, last_date, keyword_string)
 
 
 
