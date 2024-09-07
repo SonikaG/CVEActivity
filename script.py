755e5dfe78d8af27fcd92d47114d11a86d7a46fa
Sonika Garg <sonikagarg@Sonikas-MBP.lan>
2024-09-07 17:18:16 -0500

    added arguements to commandline and finish up script to work end to end

--- script.py
+++ script.py
@@ -3,23 +3,37 @@ import nvdlib
 import datetime
 from packaging.version import Version
 import sys
-
+import argparse
 from config import *
 
+'''
+function: a wrapper function for our calls to NIST API to get CPES, I use nvdlib see resources.txt
+param1: the end time, where we start time from 
+param2: last_datte where we want to stop (beginning of 2023)
+param3: keyword string we are searching for in cpes
+param4: delta is the time period we search for per api call, in this case I use 30 days
+'''
 def queryCPES(end, last_date, keyword_string, delta):
     start = end - delta
     cpes = []
     while(start > last_date):
+        print("first loop")
         cpes += (nvdlib.searchCPE(keywordSearch = keyword_string, key = NIST_API_KEY, lastModStartDate=start, lastModEndDate=end))
         start -= delta
         end -= delta
     cpes += (nvdlib.searchCPE(keywordSearch = keyword_string, key = NIST_API_KEY, lastModStartDate=last_date, lastModEndDate=end))
     cpes = set(cpes)
+    print(len(cpes))
     return cpes
 
+'''
+function: a wrapper funcion for our calls to NIST API to get CVES, I use nvdlib see resources.txt
+param1: cpes that we should search for cves from 
+'''
 def queryCVES(cpes):
     cves = {}
     for cpe in cpes: 
+        print("second loop")
         version = cpe.cpeName.split(":")[5]
         try:
             version = Version(version)
@@ -39,8 +53,13 @@ def queryCVES(cpes):
     #print(list(cves.items()))
     return cves
 
+'''
+function: a wrapper function to orchestrate all parts of initializing our database
+param1: keyword string we are searching for in cves
+param2: name of our cve table 
+'''
 def create_database(keyword_string, tableName):
-    end = datetime.datetime.now()
+    #end = datetime.datetime.now()
     end = datetime.datetime(2023, 2, 28)
     current_time = end.strftime('%Y-%m-%d %H:%M:%S')
     last_date = datetime.datetime(2023, 1, 1)
@@ -56,12 +75,18 @@ def create_database(keyword_string, tableName):
         print(Exception)
         raise()
 
-
+'''
+function: a wrapper function that orchestrates all parts of updating our database
+param1: keyword string we are searching for in cves
+param2: name of our cve table 
+'''
 def update_database(keyword_string, tableName):
+    #we need the last time we ran a cve query so we don't query this time for data we already have
     last_date = get_latest_timestamp_mysql(tableName)
     end = datetime.datetime.now()
     cpes = queryCPES(end, last_date, keyword_string, datetime.timedelta(days=30))
     cves =  queryCVES(cpes)  
+    #now we have a new time for when we last queried for cves 
     current_time = end.strftime('%Y-%m-%d %H:%M:%S')
     try:
         update_values_mysql(tableName, cves, current_time)
@@ -72,7 +97,12 @@ def update_database(keyword_string, tableName):
         print(Exception)
         raise()
     
-
+'''
+function to update our database with new cves, old cves with new version information 
+param1: name of our cve table
+param2: map of cve_id to versions
+param3: time of last cve query so that we can update that value in our Meta table 
+'''
 def update_values_mysql(tableName, cves, current_time):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
     port=MYSQL_PORT,database=MYSQL_DATABASE)
@@ -83,7 +113,7 @@ def update_values_mysql(tableName, cves, current_time):
         version = get_version_for_id(tableName, k)
         #if this row exists in database
         if version:
-            #if we can update version number
+            #if we have new version number(s)
             if len(v) > 0:
                 v.sort()
                 old_version = version.split("-")
@@ -109,6 +139,11 @@ def update_values_mysql(tableName, cves, current_time):
     conn.commit()
     conn.close()
 
+'''
+fucntion to get version information for a cve_id if it exists
+param1: the name of our cve table
+param2: id we are looking for 
+'''
 def get_version_for_id(tableName, id):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
     port=MYSQL_PORT,database=MYSQL_DATABASE)
@@ -122,20 +157,26 @@ def get_version_for_id(tableName, id):
     else:
         return None  
 
+'''
+function to insert cve and versions into our database
+param1: the name of our cve table 
+param2: a map of cve_id to version 
+'''
 def insert_values_mysql(tableName, cves):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
     port=MYSQL_PORT,database=MYSQL_DATABASE)
     cursor = conn.cursor()
     for k,v in cves.items():
+        #Do we have version numbers then?
         if len(v) == 0:
             sql = "INSERT INTO " + tableName + " (ID) VALUES (%s);"
             val = [k]
             cursor.execute(sql, val)
             conn.commit()
+        #if we do have version numbers include those in INSERT statement 
         else:
             v.sort()
             versions = str(v[0]) + "-" + str(v[-1])
-            #print(versions)
             sql = "INSERT INTO " + tableName + " (ID, Version) VALUES (%s, %s)"
             val = (k, versions)
             cursor.execute(sql, val)
@@ -143,12 +184,17 @@ def insert_values_mysql(tableName, cves):
     #in the next iteration, ideally it would be batch committed so progress isn't lost 
     conn.commit()
     conn.close()
-
+'''
+fucntion to create our database and a meta database
+param1: name of the database that stores our cve data
+param2: we want to store the time we queried the cves so that on update we startt from that time
+'''
 def create_database_mysql(tableName, current_time):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
     port=MYSQL_PORT,database=MYSQL_DATABASE)
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS " +  tableName + " (ID varchar(255) NOT NULL, Version varchar(255), PRIMARY KEY (ID));")
+    #NOTE: This is where we also create a Meta table. This is to keep track of the last time we updated the cve table (or any table)
     cursor.execute("CREATE TABLE IF NOT EXISTS Meta (Tablename varchar(255), lastUpdated TIMESTAMP);")
     sql = ("INSERT INTO Meta (Tablename, lastUpdated) VALUES (%s, %s)")
     val = [tableName, current_time]
@@ -156,6 +202,10 @@ def create_database_mysql(tableName, current_time):
     conn.commit()
     conn.close()
 
+'''
+Funciton to get the latest update timestamp from the database for a given table
+param1 tableName: the table we want the lastUpdate time for
+'''
 def get_latest_timestamp_mysql(tableName):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
     port=MYSQL_PORT,database=MYSQL_DATABASE)
@@ -168,26 +218,27 @@ def drop_table(tableName):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
     port=MYSQL_PORT,database=MYSQL_DATABASE)
     cursor = conn.cursor()
-    cursor. execute("DROP TABLE " + tableName + ";")
+    cursor. execute("DROP TABLE IF EXISTS " + tableName + ";")
     conn.commit()
     conn.close()
 
-
-def containsDigit(version):
-    return any(char.isdigit() for char in version)
-
-
 def main():
     print("here")
-    '''end = datetime.datetime.now()
-    end = datetime.datetime(2023, 2, 28)
-    current_time = end.strftime('%Y-%m-%d %H:%M:%S')
-    last_date = datetime.datetime(2023, 1, 1)'''
     keyword_string = 'java-merge-sort'
     tableName = 'cve'
-    create_database(keyword_string, tableName)
-    '''create_database_mysql('cve', current_time)
-    insert_values_mysql('cve', cves)'''
+    #from here: https://stackoverflow.com/questions/8259001/python-argparse-command-line-flags-without-arguments
+    parser = argparse.ArgumentParser(description='create or update nist database with java packages')
+    parser.add_argument('action', help='please provide either create or update')
+    args = parser.parse_args()
+    action = args.action
+    print(action)
+    if not (action == "create" or action == "update"):
+        print("action arguementt can only be \"create\" or \"update\"")
+        sys.exit()
+    if (action == "create"):
+        create_database(keyword_string, tableName)
+    elif (action == "update"):
+        update_database(keyword_string, tableName)
 
 # Using the special variable 
 # __name__
