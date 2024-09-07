ba5db1b81ff0d6129d68b68d999178556ebc8a33
Sonika Garg <sonikagarg@Sonikas-MBP.lan>
2024-09-07 17:26:19 -0500

    add config file with secrets removed

---
+++ config.py
@@ -0,0 +1,8 @@
+#from https://stackoverflow.com/questions/6198372/most-pythonic-way-to-provide-global-configuration-variables-in-config-py
+
+MYSQL_PORT = 
+MYSQL_DATABASE = ''
+MYSQL_SERVER = ''
+MYSQL_PASS = ''
+MYSQL_HOST = ''
+NIST_API_KEY = ''
