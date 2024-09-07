5e4f652b58cf2bd89ea6d7988042757f87631c28
Sonika Garg <sonikagarg@Sonikas-MBP.lan>
2024-09-07 17:23:14 -0500

    clean up test file

--- test.py
+++ test.py
@@ -11,7 +11,8 @@ def test_mysql_create_database_multiversion():
     answer = result[0]
     assert(answer[0] == "key1")
     assert(answer[1] == "0.0.5-1.0.3")
-    time = getRows('Meta')[0][1]
+    time = get_latest_timestamp_mysql('testDB')
+    print(time)
     assert(str(time) == current_time)
     print("TEST ONE PASSED")
     drop_table('testDB')
@@ -26,7 +27,7 @@ def test_mysql_create_database_two_version():
     answer = result[0]
     assert(answer[0] == "key1")
     assert(answer[1] == "1.0.0-1.0.3")
-    time = getRows('Meta')[0][1]
+    time = get_latest_timestamp_mysql('testDB')
     assert(str(time) == current_time)
     print("TEST TWO PASSED")
     drop_table('testDB')
@@ -41,7 +42,7 @@ def test_mysql_create_database_one_version():
     answer = result[0]
     assert(answer[0] == "key1")
     assert(answer[1] == "1.0.0-1.0.0")
-    time = getRows('Meta')[0][1]
+    time = get_latest_timestamp_mysql('testDB')
     assert(str(time) == current_time)
     print("TEST THREE PASSED")
     drop_table('testDB')
@@ -57,7 +58,7 @@ def test_mysql_create_database_empty_version():
     answer = result[0]
     assert(answer[0] == "key1")
     assert(answer[1] == None)
-    time = getRows('Meta')[0][1]
+    time = get_latest_timestamp_mysql('testDB')
     assert(str(time) == current_time)
     print("TEST FOUR PASSED")
     drop_table('testDB')
@@ -70,10 +71,10 @@ def test_mysql4_get_latest_timestamp():
     create_database_mysql('testDB', current_time)
     insert_values_mysql('testDB', cves)
     result = get_latest_timestamp_mysql('testDB')
-    #print(result)
     assert(str(result) == current_time)
     print("TEST FIVE PASSED")
     drop_table('testDB')
+    drop_table('Meta')
 
 #this test cases were created from looking at CVE detail here: https://nvd.nist.gov/vuln/detail/CVE-2022-24913#range-12568480
 def test_query_cpes_and_cves_valid_string():
@@ -146,13 +147,6 @@ def test_mysql_update_values_correct_version2():
     drop_table('testDB')
     drop_table('Meta')
 
-def drop_table(tableName):
-    conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
-    port=MYSQL_PORT,database=MYSQL_DATABASE)
-    cursor = conn.cursor()
-    cursor. execute("DROP TABLE " + tableName + ";")
-    conn.commit()
-    conn.close()
 
 def getRows(tableName):
     conn = mysql.connector.connect(user=MYSQL_SERVER, password=MYSQL_PASS, host=MYSQL_HOST, 
@@ -174,3 +168,5 @@ test_query_cpes_and_cves_no_result()
 test_mysql_get_version_with_id()
 test_mysql_update_values_correct_version1()
 test_mysql_update_values_correct_version2()
+drop_table('testDB')
+drop_table('Meta')
